1360632b75a46d7eab244a59826dfbc6a6454496
Dan <dan@nectry.com>
2024-05-21 22:34:23 -0400

    remove unused C functions

--- src/c/ajaxUpload.c
+++ src/c/ajaxUpload.c
@@ -2,24 +2,6 @@
 
 #include <ajaxUpload.h>
 
-uw_Basis_string uw_AjaxUploadFfi_tweakForm(uw_context ctx, uw_Basis_bool autoSubmit, uw_Basis_string iframeId, uw_Basis_string submitId) {
-  return uw_Basis_mstrcat(ctx,
-                          "<iframe id=\"",
-                          iframeId,
-                          "\" name=\"",
-                          iframeId,
-                          "\" src=\"about:blank\" style=\"width:0;height:0;border:0px solid #fff;\"></iframe>\n<script type=\"text/javascript\">var subm = document.getElementById(\"",
-                          submitId,
-                          "\"); subm.parentNode.target = \"",
-                          iframeId,
-                          "\"; var onSub = subm.onblur; subm.onblur = undefined; subm.parentNode.onsubmit = function() { window.top.event = {}; onSub(); return true; }; subm.withHandle = subm.onkeydown; subm.onkeydown = undefined; subm.withError = subm.onfocus; subm.onfocus = undefined; ",
-                          autoSubmit
-                          ? "subm.style.visibility = \"hidden\"; for (var node = subm.previousSibling; node.tagName != \"INPUT\"; node = node.previousSibling); node.onchange = function() { subm.parentNode.submit(); }; "
-                          : "",
-                          "</script>",
-                          NULL);
-}
-
 uw_Basis_string uw_AjaxUploadFfi_notifySuccess(uw_context ctx, uw_Basis_string submitId, uw_Basis_int handle) {
   uw_Basis_string r = uw_Basis_mstrcat(ctx,
                           "<script type=\"text/javascript\">var subm = parent.document.getElementById(\"",
@@ -37,10 +19,6 @@ uw_Basis_string uw_AjaxUploadFfi_notifyError(uw_context ctx, uw_Basis_string sub
                           submitId,
                           "\"); parent.event = {}; subm.withError(); </script>",
                           NULL);
-}
-
-uw_Basis_string uw_AjaxUploadFfi_idToString(uw_context ctx, uw_Basis_string s) {
-  return s;
 }
 
 uw_Basis_string uw_AjaxUploadFfi_stringToId(uw_context ctx, uw_Basis_string s) {
